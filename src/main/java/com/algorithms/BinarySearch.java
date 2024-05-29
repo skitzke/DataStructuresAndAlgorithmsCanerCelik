3439c296f86664af80c283bbf12c9a02b24e9889
skitzke <egslotz@gmail.com>
2024-05-29 19:33:24 +0200

    + Added BinarySearch class + Implemented static search method to perform binary search on an array of integers + The search method returns the index of the target value if found, or -1 if not found.

--- src/main/java/com/algorithms/BinarySearch.java
+++ src/main/java/com/algorithms/BinarySearch.java
@@ -1,4 +1,20 @@
 package com.algorithms;
 
 public class BinarySearch {
+    public static <E extends Comparable<E>> int search(E[] array, E key) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid].compareTo(key) < 0) {
+                low = mid + 1;
+            } else if (array[mid].compareTo(key) > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1; // Key not found
+    }
 }
