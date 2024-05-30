bd2288d9363dd365bf6499c4c7825793c61fd6a4
skitzke <egslotz@gmail.com>
2024-05-30 14:26:56 +0200

    + Implemented static sort method to perform quicksort on an array of integers. + The sort method sorts the array in ascending order using the quicksort algorithm.

--- src/main/java/com/algorithms/QuickSort.java
+++ src/main/java/com/algorithms/QuickSort.java
@@ -1,4 +1,32 @@
 package com.algorithms;
 
 public class QuickSort {
+    public static <E extends Comparable<E>> void sort(E[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void quickSort(E[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] array, int low, int high) {
+        E pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                E temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        E temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
 }
