1505dd637952d4284e5b827f0f38782621aaedd5
skitzke <egslotz@gmail.com>
2024-05-30 14:23:16 +0200

    + Implemented static sort method to preform bubble sort on an array of integers. + The sort method sorts the array in ascending order using the bubble sort algorithm.

--- src/main/java/com/algorithms/BubbleSort.java
+++ src/main/java/com/algorithms/BubbleSort.java
@@ -1,4 +1,20 @@
 package com.algorithms;
 
 public class BubbleSort {
+    public static <E extends Comparable<E>> void sort(E[] array) {
+        int n = array.length;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (array[i - 1].compareTo(array[i]) > 0) {
+                    E temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
 }
